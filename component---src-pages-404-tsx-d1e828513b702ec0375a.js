46930acf6c98fa5f5e40b464c29fd3ae94b54488
nanzm <nanzm@users.noreply.github.com>
2021-12-22 01:04:39 +0000

    deploy: nanzm/blog@dc881df3100734ec0f9078f1a2add56b37a8b372

---
+++ component---src-pages-404-tsx-d1e828513b702ec0375a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[218],{9786:function(t,e,a){a.r(e);var n=a(7294),l=a(4369),o=a(471);e.default=function(t){var e=t.data,a=t.location,u=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:u},n.createElement(o.Z,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-d1e828513b702ec0375a.js.map
